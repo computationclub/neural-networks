720dffcb8fc9caf443ac9abc67dff630fb6f7d0f
James Adam <james@lazyatom.com>
2016-05-31 14:37:47 +0100

    Comment information is now in the README

--- net.rb
+++ net.rb
@@ -156,38 +156,6 @@ class NeuralNetwork
 end
 
 if __FILE__ == $0
-  # Code that's useful to actually run and train the network. Options are
-  # available to use different network procs, and to load/store networks from
-  # different files. This is useful to compare performance of, say, different
-  # numbers of medial neurons, or the presence/different values for the bias
-  # neuron.
-  #
-  # For example:
-  #
-  #    $ ruby net.rb -n polar -t 10000
-  #    [10000] error: 0.0197513783712034125
-  #    Average error is BETTER (0.018645820043898788 => 0.018576975078352542)
-  #
-  # ... will create a new neural network to solve the polar problem, train it
-  # for 10,000 iterations, and then save the resulting network in `polar.yml`.
-  # It also outputs the error as the training runs, and at the end, a summary
-  # of the change in error between the start of training and the end of
-  # training.
-  #
-  # If you run this command again, it will load the network from that file, and
-  # train it for an additional 10,000 iterations.
-  #
-  # To test the network, run it without the `-t` argument and with the
-  # coordinates instead:
-  #
-  #    $ ruby net.rb -n polar 0.5 1.25
-  #    [0.5, 1.25] => [0.16715159257595702, 0.46906771983462636]
-  #          expected [0.15766118119763434, 0.4744923096777931]
-  #        difference [-0.009490411378322683, 0.005424589843166738]
-  #
-  # ... will output the result of the network, along with the actual values
-  # and a summary of the errors
-
   require 'optparse'
 
   # Computation configuration for various different applications. Remember that
